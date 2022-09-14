37515a0b826999ae00cfaad02b1029fbdb1d4d89
Siya Choudhary <67403730+siyachoudhary@users.noreply.github.com>
2022-09-14 18:15:08 -0500

    getUserDataComplete

---
+++ app/src/main/java/com/example/healthapp/GetUserPreferences.java
@@ -0,0 +1,19 @@
+package com.example.healthapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class GetUserPreferences extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_user_preferences);
+    }
+
+    public void completeSetUp(View view){
+
+    }
+}
