3f3babc9b659c43c4d2c8c6329fc2f17a2b20194
siyasiyasiya <siyacj08@gmail.com>
2022-09-15 20:05:40 -0500

    added images to UI for home page

--- app/src/main/java/com/example/healthapp/HomePage.java
+++ app/src/main/java/com/example/healthapp/HomePage.java
@@ -5,7 +5,34 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class HomePage extends AppCompatActivity {
-
+    String[] fitnessFactArr = {"You use 200 muscles to take a single step",
+                                "The pressure on your feet when you run is as much as 4 times your body weight",
+                                "Eating protein before your workout increases gains in muscle mass",
+                                "The average person walks about 70,000 miles in their lifetime",
+                                "Working out increases your lifespan",
+                                "It takes at least 12 weeks of regular exercise to get into shape",
+                                "Switching up your workout will help you lose more weight",
+                                "Being dehydrated impairs your exercise performance",
+                                "People who don’t exercise regularly can lose 80% of their strength by age 65",
+                                "Walking briskly burns nearly as many calories as jogging",
+                                "Staying active reduces your risk of many cancers",
+                                "You’re never too old to benefit from exercise",
+                                "Regular exercise improves your mental health",
+                                "You’re more productive when you’re active",
+                                "It only takes 2.5 hours of moderate physical activity to see cardiovascular benefits",
+                                "People who exercise regularly have higher vitamin D levels in their blood.",
+                                "Regular exercise can prevent illness",
+                                "or every pound of muscle gained, the body burns 50 extra calories every day",
+                                "People burn an average of 50 calories every hour while they sleep",
+                                "",
+                                "",
+                                "",
+                                "",
+                                "",
+                                "",
+                                "",
+                                "",
+                                ""};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
