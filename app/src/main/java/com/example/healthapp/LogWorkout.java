9a9c305513c7e38250bb3300ddd40b00d24716af
siyasiyasiya <siyacj08@gmail.com>
2022-09-25 20:32:23 -0500

    fixed connection between pages

--- app/src/main/java/com/example/healthapp/LogWorkout.java
+++ app/src/main/java/com/example/healthapp/LogWorkout.java
@@ -5,7 +5,10 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 
 public class LogWorkout extends AppCompatActivity {
@@ -17,23 +20,37 @@ public class LogWorkout extends AppCompatActivity {
         setContentView(R.layout.activity_log_workout);
 
 
-        for (int i = 0; i < 10; i++) {
-            fragmentManager = getSupportFragmentManager();
-            if(findViewById(R.id.exerciseScrollView)!=null){
-                if(savedInstanceState!=null){
-                    return;
-                }
-
-                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-                ExerciseDisplayFragment exerciseDisplayFragment = new ExerciseDisplayFragment();
-                Button button = findViewById(R.id.viewExerciseBtn);
-                Bundle b = new Bundle();
-//                b.putString("exercise", exercises[i]);
-                exerciseDisplayFragment.setArguments(b);
-                fragmentTransaction.add(R.id.exerciseScrollView, exerciseDisplayFragment).commit();
-            }
-        }
+//        for (int i = 0; i < 10; i++) {
+//            fragmentManager = getSupportFragmentManager();
+//            if(findViewById(R.id.exerciseScrollView)!=null){
+//                if(savedInstanceState!=null){
+//                    return;
+//                }
+//
+//                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+//                ExerciseName exerciseName = new ExerciseName();
+//                Button button = findViewById(R.id.viewExerciseBtn);
+//                Bundle b = new Bundle();
+////                b.putString("exercise", exercises[i]);
+////                ExerciseName.setArguments(b);
+//                fragmentTransaction.add(R.id.exerciseDisplay, exerciseName).commit();
+//            }
+//        }
     }
 
+    public void navProfile(View v){
+        startActivity(new Intent(LogWorkout.this, UserProfile.class));
+    }
 
+    public void navExercises(View v){
+        startActivity(new Intent(LogWorkout.this, MainExcercises.class));
+    }
+
+    public void navHome(View v){
+        startActivity(new Intent(LogWorkout.this, HomePage.class));
+    }
+
+    public void navLog(View v){
+        startActivity(new Intent(LogWorkout.this, CalendarLog.class));
+    }
 }
