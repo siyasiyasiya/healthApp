662934f97967d03981890fd708c87791d6373538
siyasiyasiya <siyacj08@gmail.com>
2022-09-26 12:55:09 -0500

    presentable project might be additions

--- app/src/main/java/com/example/healthapp/CalendarLog.java
+++ app/src/main/java/com/example/healthapp/CalendarLog.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -14,12 +15,16 @@ import android.widget.Toast;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class CalendarLog extends AppCompatActivity {
     String date = "";
     DBHelper DB;
     ArrayList<ImageView> stars = new ArrayList<>();
     CalendarView calendar;
+    int year;
+    int month;
+    int day;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +33,7 @@ public class CalendarLog extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar_log);
         calendar = findViewById(R.id.calendar);
-
+        Button delete = findViewById(R.id.deleteBtn);
 
         ImageView star1 = findViewById(R.id.star1);
         ImageView star2 = findViewById(R.id.star2);
@@ -42,13 +47,39 @@ public class CalendarLog extends AppCompatActivity {
         stars.add(star4);
         stars.add(star5);
 
+        Calendar cal = Calendar.getInstance();
+        year = cal.get(Calendar.YEAR);
+        month = cal.get(Calendar.MONTH);
+        day = cal.get(Calendar.DATE);
+
+        date = month + "/" + day + "/" + year;
+
         loadData();
 
         calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
-            public void onSelectedDayChange(CalendarView view, int year, int month,
-                                            int dayOfMonth) {
-                date = month + "/" + dayOfMonth + "/" + year;
+            public void onSelectedDayChange(CalendarView view, int y, int m,
+                                            int d) {
+                date = m + "/" + d + "/" + y;
+                month = m;
+                year = y;
+                day = d;
+
+                loadData();
+            }
+        });
+
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Boolean checkDeleteData = DB.deleteData(date);
+
+                if (checkDeleteData==true){
+                    Toast.makeText(CalendarLog.this,"Entry Deleted",Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(CalendarLog.this,"Entry Not Deleted",Toast.LENGTH_SHORT).show();
+                }
+
                 loadData();
             }
         });
@@ -58,12 +89,14 @@ public class CalendarLog extends AppCompatActivity {
         TextView title = findViewById(R.id.title);
         TextView duration = findViewById(R.id.durationLbl);
         TextView list = findViewById(R.id.exerciseListLbl);
+        Button delete = findViewById(R.id.deleteBtn);
 
         for (ImageView star : stars) {
             star.setVisibility(View.VISIBLE);
         }
         duration.setVisibility(View.VISIBLE);
         list.setVisibility(View.VISIBLE);
+        delete.setVisibility(View.VISIBLE);
 
         try (Cursor res = DB.getData()) {
             if (res.getCount() == 0) {
@@ -73,6 +106,7 @@ public class CalendarLog extends AppCompatActivity {
                 }
                 duration.setVisibility(View.INVISIBLE);
                 list.setVisibility(View.INVISIBLE);
+                delete.setVisibility(View.INVISIBLE);
                 return;
             }
 
@@ -100,6 +134,7 @@ public class CalendarLog extends AppCompatActivity {
                 }
                 duration.setVisibility(View.INVISIBLE);
                 list.setVisibility(View.INVISIBLE);
+                delete.setVisibility(View.INVISIBLE);
             }
         }
     }
